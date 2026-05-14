0ce697030555d84bd8e4628760812f9cbdb08c14
sunwin1029 <sunwin1029@gmail.com>
2026-05-15 01:27:32 +0900

    hotfix: 탈퇴 고객센터 연결

--- src/pages/MyPage/WithdrawConfirmModal.tsx
+++ src/pages/MyPage/WithdrawConfirmModal.tsx
@@ -18,7 +18,9 @@ function WithdrawConfirmModal({ onConfirm, onCancel, isLoading }: WithdrawConfir
       >
         {/* 내용 */}
         <div className="flex flex-col items-center gap-[0.9375rem]">
-          <p className="typo-subtitle-header-2 text-grey-900">정말 탈퇴하시겠어요?</p>
+          <p className="typo-subtitle-header-2 text-grey-900">
+            탈퇴는 도미사 고객센터로 문의해주세요
+          </p>
           <div className="flex flex-col items-center gap-1">
             <p className="typo-input-text-m text-warning-ac">탈퇴 시 내정보와 소개팅 카드,</p>
             <p className="typo-input-text-m text-warning-ac flex items-center gap-1">
@@ -36,7 +38,7 @@ function WithdrawConfirmModal({ onConfirm, onCancel, isLoading }: WithdrawConfir
             disabled={isLoading}
             className="flex flex-1 h-[3.125rem] items-center justify-center rounded-[0.875rem] bg-grey-400 typo-button-text-b text-grey-800 disabled:opacity-50"
           >
-            {isLoading ? '탈퇴 중' : '탈퇴할래요'}
+            {isLoading ? '이동 중' : '문의하기'}
           </button>
           <button
             type="button"
