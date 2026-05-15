5aedacaa931c1db7b7f422d4146dc0d8920728de
sunwin1029 <sunwin1029@gmail.com>
2026-05-15 17:44:43 +0900

    hotfix: vercel GA 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 // import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
@@ -11,8 +12,11 @@ resetRestoredLocalhostRoute();
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>
+  <>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+    <Analytics />
+  </>
   // </StrictMode>,
 );
